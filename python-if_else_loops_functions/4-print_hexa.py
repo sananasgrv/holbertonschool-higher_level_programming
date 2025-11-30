a533d8e46b4a45412bb0321db00a12659da1d85f
sananasgrv <sanan.asgarov2023@khazar.org>
2025-11-30 18:37:31 +0000

    .

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-decimal=0
 for decimal in range(98):
-    hexi=hex(decimal)
-    print("{0} = {1}".format(decimal,hexi))
-
+    hexi = hex(decimal)
+    print("{0} = {1}".format(decimal, hexi))
