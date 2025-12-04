3553fd9291b30eb8ad43184125859d0213d5ab36
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-04 18:20:19 +0400

    .

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    result = a+b
-    return print(f"{a} + {b} = {result}")
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
