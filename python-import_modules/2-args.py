6f2d8156e4b0b237bc9cf53ffd5a0497aa790484
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-05 16:47:05 +0400

    commit 4

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
-    if len(list) > 1:
+    if len(argv) > 1:
         print("{:d} arguments:".format(len(argv)))
         for i in range(len(argv)):
-            print("{:d}: {}".format(i,argv[i]))
+            print("{:d}: {}".format(i+1,argv[i]))
     elif len(argv) == 1:
         print("1 argument:")
-        print("{}".format(dir(argv)))
-    elif len(argv) == 0:
+        print("{}".format(argv[0]))
+    else:
         print("0 argumnts.")
