5a4c8af3bd8bae64f914acf0f94c6a729bc7c601
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-05 16:40:30 +0400

    commit 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-def arg():
-    list=dir()
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
     if len(list) > 1:
-        print("{:d} arguments:".format(len(list)))
-        for i in range(len(list)):
-            print("{:d}: {}".format(i,list[i]))
-    elif len(list) == 1:
-        print("{:d} argument:".format(len(list)))
-        print("{}".format(dir(list)))
-    elif len(list) == 0:
-        print("{:d} argumnts.")
-    return 0
+        print("{:d} arguments:".format(len(argv)))
+        for i in range(len(argv)):
+            print("{:d}: {}".format(i,argv[i]))
+    elif len(argv) == 1:
+        print("1 argument:".format(len(argv)))
+        print("{}".format(dir(argv)))
+    elif len(argv) == 0:
+        print("0 argumnts.")
+
