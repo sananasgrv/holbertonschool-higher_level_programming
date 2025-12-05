4062a9e90ff9914e8228f26a433e261b95a6ef6a
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-05 16:58:39 +0400

    commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if len(argv) > 1:
         print("{:d} arguments:".format(len(argv)))
         for i in range(len(argv)):
-            print("{:d}: {}".format(i+1,argv[i]))
+            print("{:d}: {}".format(i+1, argv[i]))
     elif len(argv) == 1:
         print("1 argument:")
         print("1: {}".format(argv[0]))
     else:
-        print("0 argumnts.")
+        print("0 arguments.")
