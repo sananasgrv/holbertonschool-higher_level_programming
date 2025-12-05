cedcdcde808b9746345bd58707b01537f17ef2b7
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-05 17:18:26 +0400

    commit message

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     SUM = 0
-    for i in range(len(argv)):
-        SUM += int(i)
-        print(SUM)
+    for i in range(1, len(argv)):
+        SUM += int(argv[i])
+    print(SUM)
