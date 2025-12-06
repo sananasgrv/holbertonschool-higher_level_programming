55213bc57761fafda80adb655c7562cfec1f6da7
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-06 19:27:41 +0400

    commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(matrix):
-        for j in range(matrix):
+    for i in range(len(matrix)):
+        print("")
+        for j in range(len(matrix[0])):
             print("{:d}".format(matrix[i][j]), end=" ")
+
