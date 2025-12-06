923ef505c4f98138cfc04b8e4c6e9d9eb08b565e
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 01:52:02 +0400

    commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = my_list[0]
-    if len(my_list) != 0:
-        for i in my_list:
-            if i > max_value:
-                max_value = i
-        return max_value
-    else:
+    if len(my_list) == 0:
         return None
+
+    max_value = my_list[0]
+    for i in my_list:
+        if i > max_value:
+            max_value = i
+    return max_value
