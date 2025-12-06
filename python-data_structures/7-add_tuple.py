bb241ac92caa65d07e6404bc780c5ced78934ac3
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 01:29:57 +0400

    commit

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
-    new_list=[]
+    new_list = []
     if len(list_a) < 2 or len(list_b) < 2:
         for i in range(2):
             list_a.append(0)
