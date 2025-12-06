b59cdd63dc4b569f08ed0da50fb1b1b58102ab6b
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 01:36:56 +0400

    commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,5 +3,5 @@ def multiple_returns(sentence):
     if len(sentence) != 0:
         lenght = len(sentence)
         first = sentence[0]
-        tuple = (lenght, first)
-        return tuple()
+        my_tuple = (lenght, first)
+        return my_tuple
