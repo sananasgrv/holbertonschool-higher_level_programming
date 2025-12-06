0519811ad1514ff4b3bd5e352cde856af71de503
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 01:57:05 +0400

    commit

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_result=[]
+    for i in my_list:
+        if i%2 == 0:
+            return list_result.append(True)
+        else:
+            return list_result(False)
