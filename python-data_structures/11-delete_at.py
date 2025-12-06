93a2f43f8ef966cb1f8b61aef6e2984d5f467bcf
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 02:17:51 +0400

    commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,8 +3,8 @@ def delete_at(my_list=[], idx=0):
     new_list = []
     if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        for i in range(len(my_list)):
-            if i != idx:
-                new_list.append(my_list[i])
-        return new_list
+
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    return new_list
