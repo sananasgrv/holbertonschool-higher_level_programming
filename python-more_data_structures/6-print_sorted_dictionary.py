8bc1bfe43a956db1442f9ffd6bc5d994821b7f22
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 15:49:31 +0400

    commit

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     myKeys = list(a_dictionary.keys())
     myKeys.sort()
     sd = {i: a_dictionary[i] for i in myKeys}
-    return sd
+    for i in sd.keys():
+        print("{} : {}".format(i,sd.get(i)))
