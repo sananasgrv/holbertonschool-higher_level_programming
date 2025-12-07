8964b204a6d6144d10b25015d94cba36820b708a
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 18:24:11 +0400

    commit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rome_dict = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     prev_value = 0
 
