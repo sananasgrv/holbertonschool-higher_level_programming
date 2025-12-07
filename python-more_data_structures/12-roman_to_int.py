2c240a4c736c6ee56ae5366fa7b4c8dde8b6b1e1
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 18:22:07 +0400

    commit

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,17 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return None
-    rome_dict= {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50 ,'C' : 100, 'D' : 500, 'M' : 1000}
+
+    rome_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    for i in range(len(roman_string)):
-        if roman_string[i] >= roman_string[i-1]:
-            num += rome_dict.get(roman_string[i])
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        value = rome_dict.get(char, 0)
+        if value < prev_value:
+            num -= value
         else:
-            num -= rome_dict.get(roman_string[i])
+            num += value
+        prev_value = value
+
     return num
