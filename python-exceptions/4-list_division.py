d9ceb8331db4fddd4cebe858b2ec5cd6c8d4c36a
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-07 19:59:01 +0400

    commit

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError
-            if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+            if not isinstance(my_list_1[i],
+                              (int, float)) or not isinstance(my_list_2[i], (int, float)):
                 raise TypeError
             result.append(my_list_1[i] / my_list_2[i])
         except TypeError:
