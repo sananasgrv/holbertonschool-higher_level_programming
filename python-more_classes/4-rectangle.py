977aa72e91f1c227ca61e7db027dd4d05d23c9c4
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-10 18:19:54 +0400

    commit

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -52,4 +52,4 @@ class Rectangle:
         return result
 
     def __repr__(self):
-        print(f"Rectangle({self.__width}, {self.__height})")
+        return f"Rectangle({self.__width}, {self.__height})"
