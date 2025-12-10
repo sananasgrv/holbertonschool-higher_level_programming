489adb96d248579f57174851ffa8bf6f2f44b2a0
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-10 18:12:42 +0400

    commit

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,8 +46,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return result
         for i in range(self.__height):
-            result+= "#" * self.__width
+            result += "#" * self.__width
             if i < self.__height - 1:
                 result += "\n"
         return result
-
