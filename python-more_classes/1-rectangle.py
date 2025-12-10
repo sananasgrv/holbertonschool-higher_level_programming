d70e47a6945e57a6748124c8379b159a46bea6a3
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-10 16:37:00 +0400

    commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,21 +5,27 @@
 class Rectangle:
     """Class doc"""
     def __init__(self,width = 0, height = 0):
-        self.width = width
-        self.height = height
-
+        self.__width = width
+        self.__height = height
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
     def width(self, value):
         if not isinstance(value,int):
            raise(TypeError("width must be an integer"))
-        elif(self.width <0 ):
+        elif(self.__width <0 ):
            raise(ValueError("width must be >= 0"))
-        else:
-            return self.width
+        self.__width = value
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise (TypeError("width must be an integer"))
-        elif (self.height < 0):
+        elif (self.__height < 0):
             raise (ValueError("width must be >= 0"))
-        else:
-            return self.height
+        self.__height = value
+
 
