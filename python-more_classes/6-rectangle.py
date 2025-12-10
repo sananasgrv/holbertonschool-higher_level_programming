510db568cff79d681ec6e3d9b44c1fc5a018d7f0
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-10 19:03:59 +0400

    commit

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -3,7 +3,7 @@
 
 class Rectangle:
     """Class doc"""
-    number_of_instances = None
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
