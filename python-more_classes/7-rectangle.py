7949498e19ede5815a37f958194316135a7cacf9
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-10 19:27:25 +0400

    commit

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -10,7 +10,6 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.print_symbol = "#"
         Rectangle.number_of_instances += 1
 
     @property
