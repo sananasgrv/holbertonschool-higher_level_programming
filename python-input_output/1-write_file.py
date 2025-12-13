728eae4c7f9ea040d0a30b1e0b6d3f12258a72aa
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 17:18:59 +0400

    commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """Documented"""
 
-    with open(filename, "w", encoding="etf-8")  as f:
+    with open(filename, "w", encoding="utf-8")  as f:
         return f.write(text)
