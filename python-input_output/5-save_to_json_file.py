40acc0f2b5a87cef2a682fc54138b57cbb520972
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 17:33:49 +0400

    commit

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Doc"""
     with open(filename, "a", encoding="utf-8") as f:
-        return f.write(json.loads(my_obj))
+        json.dumps(my_obj)
