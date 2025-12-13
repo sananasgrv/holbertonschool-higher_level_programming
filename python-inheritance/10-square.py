4e03198e9dcde1f0b1864a94719a38d1c02f1d01
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 16:45:27 +0400

    commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Rectangle from BaseGeometry"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+    def area(self):
+        return pow(self.size, 2)
