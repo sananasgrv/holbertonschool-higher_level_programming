d1a12268dd236ad2143568c2294bdd6c914f6a90
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 13:59:33 +0400

    commit

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Documentation of py folder"""
+
+
+class BaseGeometry:
+    """Documentation of class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
