e0b8d4c443850cf182b0d7c96c63af2283d1ec24
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 13:35:01 +0400

    commit

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Documentation"""
+
+
+
 def is_kind_of_class(obj, class_a):
+    """Documentation"""
+
     obj_class = type(obj)
     if issubclass(obj_class, class_a):
         return True
