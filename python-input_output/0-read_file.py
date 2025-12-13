f619f424a034a635c1735c26758cd2db34bccff6
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 16:59:08 +0400

    commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Documentation"""
 
+
 def read_file(filename=""):
     """FUnction documentation"""
+
     with (open(filename, encoding="utf-8") as f):
         data = f.read()
         return data
