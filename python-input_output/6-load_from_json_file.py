a98758cf4d6a79a6d07d0b41d85eab05515e6777
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 18:09:45 +0400

    commit

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Doc"""
+import json
+
+
+def load_from_json_file(filename):
+    """Doc"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        json.loads(f.read())
