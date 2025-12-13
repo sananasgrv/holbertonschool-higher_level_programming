3a7139a9e4dbee4b5a315201a886c55ecbbe486f
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 13:50:34 +0400

    commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Documentation of py folder"""
+
+
+class BaseGeometry:
+    """Documentation of class"""
+    def area(self):
+        raise Exception ("area() is nor implemented")
