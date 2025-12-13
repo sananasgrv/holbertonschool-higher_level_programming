bc6dade51f334851ab6131854aa252a74b122abf
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 18:52:59 +0400

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""Documentation"""
+"""Adds all arguments to a Python list, then saves them to a JSON file"""
+
+import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-save_to_json_file(load_from_json_file(),filename)
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
