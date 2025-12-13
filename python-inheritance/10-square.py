c2132d3f91bf816b4126f025e32508e1b32f3fa5
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 16:46:17 +0400

    commit

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,5 +9,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
+
     def area(self):
         return self.__size * self.__size
