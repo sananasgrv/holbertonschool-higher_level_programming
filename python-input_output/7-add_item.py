923df8f8670f15b76464740ef66c032636eeac6d
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 18:44:34 +0400

    commit

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Doc"""
+import json
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+save_to_json_file(filename)
