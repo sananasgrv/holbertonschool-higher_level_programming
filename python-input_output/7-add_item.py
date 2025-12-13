809bf25990ed8dccb3e8cbcd874017f52c46ed62
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-13 18:54:05 +0400

    commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,11 +8,5 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
-
-items.extend(sys.argv[1:])
-
+items = load_from_json_file(filename)
 save_to_json_file(items, filename)
