caeec029a85d2b768678956b71dbbd20e520f5e1
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-14 17:55:24 +0400

    commit

---
+++ python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Doc"""
+
+
+class Student():
+    """Doc of class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self,  attrs=None):
+        if type(attrs) is list and all(type(attrs(a) is str for a in attrs)):
+            return self.__dict__
