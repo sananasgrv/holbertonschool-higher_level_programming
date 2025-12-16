7515fccd08b69d3ca8f7ef20d9f158fa97722761
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-16 12:44:32 +0400

    commit

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,9 +2,11 @@ import pickle
 
 
 def serialize_and_save_to_file(data, filename):
-    # Your code here to serialize and save data to the specified file
-    with open(filename, "wb") as file:
-        return pickle.dump(data, file)
+    try:
+        with open(filename, "wb") as file:
+            return pickle.dump(data, file)
+    except Exception as e:
+        raise TypeError("Object is not serializable") from e
 
 
 def load_and_deserialize(filename):
