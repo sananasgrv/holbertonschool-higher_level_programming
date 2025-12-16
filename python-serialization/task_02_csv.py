c4f1a7d7130514cb900d0b901eca97f941d6729f
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-16 14:20:20 +0400

    commit

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -9,8 +9,8 @@ def convert_csv_to_json(csv_file, json_file="data.json"):
         with open(csv_file, newline="", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             data = list(reader)
-        with open(json_file, "wb") as file:
-            pickle.dump(data, file, indent=4)
+        with open(json_file, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
         return True
-    except Exception as e:
+    except Exception:
         return False
