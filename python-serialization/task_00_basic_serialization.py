fb3f0992370fe7a29556d5ab4d398efb6d427f18
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-16 12:36:51 +0400

    commit

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,10 +3,10 @@ import pickle
 
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
-    with open(filename, "w") as file:
+    with open(filename, "wb") as file:
         return pickle.dump(data, file)
 
 
 def load_and_deserialize(filename):
-    with open(filename, "r") as file:
+    with open(filename, "rb") as file:
         return pickle.load(file)
