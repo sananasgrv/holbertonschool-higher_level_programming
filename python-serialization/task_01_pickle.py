9a402e712612dbc831e0b179cf4783bc0537ac84
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-16 12:57:25 +0400

    commit

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Doc"""
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name: str, age: int, is_student: bool):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+    def display(self):
+        print(f"Name: {self.name}",
+              f"Age: {self.age}",
+              f"is_studen: {self.is_student}")
+    def  serialize(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump(self)
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as file:
+            pickle.load(cls)
