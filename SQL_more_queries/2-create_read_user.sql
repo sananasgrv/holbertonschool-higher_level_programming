0e85f6a03a0547f95a0794563cd04d8bf834a759
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-21 02:51:26 +0400

    Commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,3 @@
 -- Creating read only user
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT *.* TO 'user_0d_2'@'localhost';
