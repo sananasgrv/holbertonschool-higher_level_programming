4252173ba27ac15082ff0ff95b8641b098b32a2f
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-21 02:45:36 +0400

    Commit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creating user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1pwd'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
