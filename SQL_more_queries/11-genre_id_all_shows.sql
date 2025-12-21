77cf8e2c8ce4863bceeecdb0c9d0e80c5bdc637f
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-21 16:45:14 +0400

    Commit

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- All shows
 SELECT s.title, g.genre_id
-FROM JOIN tv_shows AS s
-LEFT tv_show_genres AS g
+FROM tv_shows AS s
+JOIN LEFT tv_show_genres AS g
 ON g.show_id = s.id
 ORDER BY s.title, g.genre_id
