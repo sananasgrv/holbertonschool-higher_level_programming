9cae1ac1c9b3b22e2068f8c5d294cc628b6ee447
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-21 19:53:58 +0400

    Commit

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
---Listing
-SELECT g.name AS 'genre' , count(*) AS 'number_of_shows'
+-- Listing
+SELECT g.name AS genre , count(*) AS number_of_shows
 FROM tv_genres AS g
 INNER JOIN tv_show_genres AS sg
 ON g.id = sg.genre_id
-GROUP BY sg.show_id DESC;
+GROUP BY g.id
+ORDER BY COUNT(*) DESC;
