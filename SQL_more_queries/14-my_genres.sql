f3955bc34d1dd26080dda96a3372ef59da8eb4ca
sananasgrv <sanan.asgarov2023@khazar.org>
2025-12-21 20:23:48 +0400

    Commit

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- List the shows
+SELECT g.name
+FROM tv_show_genres AS sg
+INNER JOIN tv_shows AS s
+ON s.id = sg.show_id
+INNER JOIN tv_genres AS g
+ON sg.genre_id = g.id
+WHERE s.name = "Dexter"
